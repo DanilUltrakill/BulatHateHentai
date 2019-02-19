79713640de579e130342b0c0194e34b987ccba72
Bulat <yaremakadanil@gmail.com>
2019-02-19 12:08:54 +0700

    master commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Darkness My Old Fiend");
-            Console.ReadKey();
+            int i;
+
+            {
+
+                {
+                    Console.Write("Меню:\n1) Введите A \n2) Введите B \n3) Сложение \n4) Вычитание \n5) Умножение \n6) Деление \n7) Выход");
+                    i = int.Parse(Console.ReadLine());
+                    switch (i)
+                    {
+                        //case 1:
+                        //    Console.WriteLine("1");                           
+                        //    break;
+                        //case 2:
+                        //    Console.WriteLine("2");  
+                        //    break;
+                        //case 3:
+                        //    Console.WriteLine("3");
+
+                        //    break;
+                        //case 4:
+                        //    Console.WriteLine("4");
+                        //    break;
+                        //case 5:
+                        //    Console.WriteLine("5");
+                        //    break;
+                        //case 6:
+                        //    Console.WriteLine("6");
+                        //    break;
+                        default:
+                            Console.WriteLine("");
+                            break;
+                    }
+                    Console.Write("\nНажмите любую клавишу...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
         }
     }
 }
